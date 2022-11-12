e1cc7612b9896d0274d9f1d632e5dcbccbee9ce0
Farukov <vladislavfarukov2004@gmail.com>
2022-11-12 10:51:32 +0700

    Добавьте файлы проекта.

---
+++ ConsoleAuthorization/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAuthorization
+{
+	public class Helper
+	{
+		private static Model.LibraryBase DBEntities;
+		public static Model.LibraryBase getContex()
+		{
+			if(DBEntities == null)
+            {
+				DBEntities = new Model.LibraryBase();
+			}
+			return DBEntities;
+		}
+
+		public static void CreateUsers(Model.Client client)
+        {
+			DBEntities.Client.Add(client);
+			DBEntities.SaveChanges();
+		}
+
+		public static int GetLastID()
+        {
+			int id = 0;
+
+			try
+			{
+				id = DBEntities.Client.OrderByDescending(client => client.CL_ID).First().CL_ID;
+				return id + 1;
+			}
+            catch (System.InvalidOperationException)
+            {
+				id = 0;
+				return id + 1;
+			}
+		}
+	}
+}
---
+++ ConsoleAuthorization/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HashPassword;
+
+namespace ConsoleAuthorization
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Model.LibraryBase bd = Helper.getContex();
+				Console.WriteLine("Создание новой учетной записи для пользователя");
+				Console.Write("Введите фамилию: ");
+				string lName = Console.ReadLine();
+				Console.Write("Введите имя: ");
+				string fName = Console.ReadLine();
+				Console.Write("Введите отчество: ");
+				string patronymic = Console.ReadLine();
+				Console.Write("Введите адрес электронной почты: ");
+				string email = Console.ReadLine();
+				Console.Write("Введите дату рождения: ");
+				string dateBirth = Console.ReadLine();
+				Console.Write("Введите пароль: ");
+				string password = Console.ReadLine();
+
+				if (string.IsNullOrEmpty(lName) || string.IsNullOrEmpty(fName) ||
+					string.IsNullOrEmpty(email) || string.IsNullOrEmpty(dateBirth) || string.IsNullOrEmpty(password))
+				{
+					Console.WriteLine("\nВведены не все данные пользователя.\n");
+				}
+				else if (lName.All(char.IsLetter) || fName.All(char.IsLetter) || int.TryParse(email, out int i))
+				{
+					Console.WriteLine("\nФамилия, имя, отчество, email не могут быть числом.\n");
+				}
+				else if (password.Length > 25)
+				{
+					Console.WriteLine("\nПароль слишком длинный.\n");
+				}
+
+				else if (DateTime.TryParseExact(dateBirth, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime dt))
+				{
+					Hash hash = new Hash();
+					password = hash.Sha256Hash(password);
+					Console.WriteLine($"Хешированный пароль пользователя: {password}");
+
+					int id = Helper.GetLastID();
+
+					Model.Client client = new Model.Client { CL_ID = id, CL_LastName = lName, CL_FirstName = fName, CL_Patronymic = patronymic, CL_Email = email, CL_DateOfBirdth = dt, CL_Password = password};
+					Helper.CreateUsers(client);
+
+					Console.WriteLine("Учетная запись добавлена\n");
+				}
+                else
+                {
+					Console.WriteLine("\nДата рождения пользователя введена неверно.\n");
+                }
+			}
+		}
+	}
+}
